6b7f0268472cb956e69cd2237d73e701789ec424
Javaguy78 <cschalk@themaskedcrusader.com>
2015-08-12 22:21:42 -0600

    matching main.cpp

--- SimpleLinkedList/main.cpp
+++ SimpleLinkedList/main.cpp
@@ -3,57 +3,57 @@
 
 using namespace std;
 
-SimpleLinkedList sll(1);
+SimpleLinkedList<double> sll(1.23);
 
 void testNewObject() {
-	sll.add(13);
-	sll.add(1313);
-	sll.addLast(1234);
+	sll.add(2.34);
+	sll.add(3.45);
+	sll.addLast(4.56);
 
 	cout << "Original List:" << endl;
 	cout << sll << endl;
 	cout << "Original Size: " << sll.getSize() << endl;
 }
 
-void testCopiedByCopyConstructor() {
-	SimpleLinkedList copied(sll);
-	copied.add(2999);
-	cout << "Copied via Copy Constructor" << endl;
-	cout << copied << endl;
-	cout << "Copied Size: " << copied.getSize() << endl;
-}
+// void testCopiedByCopyConstructor() {
+//	SimpleLinkedList copied(sll);
+//	copied.add(2999);
+//	cout << "Copied via Copy Constructor" << endl;
+//	cout << copied << endl;
+//	cout << "Copied Size: " << copied.getSize() << endl;
+//}
 
-void testCopiedByAssignmentOperator() {
-	SimpleLinkedList assignment;
-	assignment = sll;
-	assignment.add(5000);
-	cout << "Copied via Assignment Operator:" << endl;
-	cout << assignment << endl;
-	cout << "Assignment Size: " << assignment.getSize() << endl;
-}
+//void testCopiedByAssignmentOperator() {
+//	SimpleLinkedList assignment;
+//	assignment = sll;
+//	assignment.add(5000);
+//	cout << "Copied via Assignment Operator:" << endl;
+//	cout << assignment << endl;
+//	cout << "Assignment Size: " << assignment.getSize() << endl;
+//}
 
-void testAssignmentOfEmptyObjectToNewObject() {
-	SimpleLinkedList empty = SimpleLinkedList();
-	SimpleLinkedList emptyCopy1 = SimpleLinkedList(empty);
-	SimpleLinkedList emptyCopy3 = empty;
-	SimpleLinkedList emptyCopy17 = SimpleLinkedList(empty);
-	empty.addLast(3);
-	cout << empty << endl;
-	cout << emptyCopy1 << endl;
-	cout << emptyCopy3 << endl;
-	emptyCopy17 = emptyCopy3;
-	empty = empty; //self assignment test
-	cout << empty << endl;
-}
+//void testAssignmentOfEmptyObjectToNewObject() {
+//	SimpleLinkedList empty = SimpleLinkedList();
+//	SimpleLinkedList emptyCopy1 = SimpleLinkedList(empty);
+//	SimpleLinkedList emptyCopy3 = empty;
+//	SimpleLinkedList emptyCopy17 = SimpleLinkedList(empty);
+//	empty.addLast(3);
+//	cout << empty << endl;
+//	cout << emptyCopy1 << endl;
+//	cout << emptyCopy3 << endl;
+//	emptyCopy17 = emptyCopy3;
+//	empty = empty; //self assignment test
+//	cout << empty << endl;
+//}
 
-void scratch() {
-	Element<long> longElement(3.12L);
-	Element<float> floatElement(3.14F);
-	Element<std::string> strElement("Awesome String");
-	Element<SimpleLinkedList> sslElement(sll);
+//void scratch() {
+//	Element<long> longElement(3.12L);
+//	Element<float> floatElement(3.14F);
+//	Element<std::string> strElement("Awesome String");
+//	Element<SimpleLinkedList> sslElement(sll);
 	// I get it!
-	return;
-}
+//	return;
+//}
 
 /////////////////////////////////////////////////////////////////////////
 /// PROGRAM ENTRY POINT                                   ///////////////
@@ -62,11 +62,11 @@ void scratch() {
 int main() {
 
 	testNewObject();
-	testCopiedByCopyConstructor();
-	testCopiedByAssignmentOperator();
-	testAssignmentOfEmptyObjectToNewObject();
+//	testCopiedByCopyConstructor();
+//	testCopiedByAssignmentOperator();
+//	testAssignmentOfEmptyObjectToNewObject();
 
-	scratch();
+//	scratch();
 
 	return 0;
 }
