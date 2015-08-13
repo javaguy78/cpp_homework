1760bfcd3b40dd405824ab5cec45088276588983
Javaguy78 <cschalk@themaskedcrusader.com>
2015-08-12 22:18:02 -0600

    First attempt at full template
    Works for primitive types only..

--- SimpleLinkedList/SimpleLinkedList.h
+++ SimpleLinkedList/SimpleLinkedList.h
@@ -11,29 +11,152 @@ struct Element {
 	}
 };
 
+template<typename T>
 class SimpleLinkedList {
 public:
-	SimpleLinkedList();
-	SimpleLinkedList(int);
-	SimpleLinkedList(const SimpleLinkedList &);
-	SimpleLinkedList(SimpleLinkedList &&);
-	~SimpleLinkedList();
-	Element<int> const * getFirst() const;
-	void add(int);
-	void addLast(int);
-	void printAll() const;
-	unsigned int getSize() const;
-	SimpleLinkedList & operator=(const SimpleLinkedList &);
-	SimpleLinkedList & operator=(SimpleLinkedList &&);
-	void swap(SimpleLinkedList &);
+	SimpleLinkedList<T>() :
+		first{ nullptr },
+		last{ nullptr },
+		size{0u} {}
+
+	SimpleLinkedList<T>(T type) : SimpleLinkedList<T>() 
+	{
+		last = first = new Element<T>(type);
+		++size;
+	}
+
+	SimpleLinkedList(const SimpleLinkedList & in)
+	{
+		try 
+		{
+			if (in.first != nullptr) 
+			{
+				first = new Element<int>(in.first->data);
+				auto * tmpLocal = first;
+				auto * tmpCopy = in.first;
+				while (tmpCopy->next != nullptr) 
+				{
+					tmpCopy = tmpCopy->next;
+					tmpLocal->next = new Element<int>(tmpCopy->data);
+					tmpLocal = tmpLocal->next;
+					last = tmpLocal;
+					++size;
+				}
+			}
+		}
+		catch (...) {
+			unwind();
+			throw;
+		}
+	}
+
+	SimpleLinkedList(SimpleLinkedList && in) 
+	{
+		first = in.first;
+		last = in.last;
+		size = in.size;
+
+		in.first = nullptr;
+		in.last = nullptr;
+		in.size = 0;
+	}
+
+	~SimpleLinkedList() 
+	{
+		unwind();
+	}
+
+	Element<T> const * getFirst() const
+	{
+		return first;
+	}
+	
+	void add(T type) 
+	{
+		addLast(type);
+	}
+
+	void addLast(T type) 
+	{
+		if (!first) 
+		{
+			last = first = new Element<T>(type);
+		}
+		else 
+		{
+			last->next = new Element<T>(type);
+			last = last->next;
+		}
+	}
+
+	void printAll() const 
+	{
+		std::cout << *this << endl;
+	}
+	
+	unsigned int getSize() const 
+	{
+		return size;
+	}
+
+	SimpleLinkedList<T> & operator=(const SimpleLinkedList<T> & in) 
+	{
+		if (this != &in) 
+		{
+			SimpleLinkedList<T>(in).swap(*this);
+		}
+		return *this;
+	}
+
+	SimpleLinkedList<T> & operator=(SimpleLinkedList<T> && in)
+	{
+		using std::move;
+		first = move(in.first);
+		last = move(in.last);
+		size = move(in.size);
+		return *this;
+	}
+
+	void swap(SimpleLinkedList<T> & in)
+	{
+		using std::swap;
+		swap(this->first, in.first);
+		swap(this->last, in.last);
+		swap(this->size, in.size);
+
+	}
 
 private:
-	Element<int> * first;
-	Element<int> * last;
+	Element<T> * first;
+	Element<T> * last;
 	unsigned int size;
-	void unwind();
+
+	void unwind() 
+	{
+		auto * tmp = first;
+		while (tmp) 
+		{
+			first = first->next;
+			delete tmp;
+			tmp = first;
+		}
+	}
+
 };
 
-std::ostream & operator<<(std::ostream&, const SimpleLinkedList&);
+template<typename T>
+std::ostream & operator<<(std::ostream& out, const SimpleLinkedList<T>& sll) {
+	auto * tmp = sll.getFirst();
+	out << "{";
+	while (tmp) {
+		out << tmp->data;
+		if (tmp->next) {
+			out << ",";
+		}
+		tmp = tmp->next;
+	};
+	out << "}";
+	return out;
+}
 
 #endif
